f69cc9dd42d82bfa0a32e8ed33ca7bcfd465642f
Lynx005F <45683914+Lynx005F@users.noreply.github.com>
2021-07-13 23:14:28 +0200

    Added character/user exclusion lists

--- replace.py
+++ replace.py
@@ -15,41 +15,47 @@ import ctypes, sys
 user_master = "15402804"
 char_master = "2114060571"
 
+# These chars don't get copied
+exclude_chars = []
+exclude_users = []
+
 # In case you or sharedcache folders are at a different location you can change the paths here
 tq_dir = os.path.join(os.getenv('LOCALAPPDATA'), "CCP\EVE\c_program_files_eve_sharedcache_tq_tranquility\settings_Default")
 sisi_dir =  os.path.join(os.getenv('LOCALAPPDATA'), "CCP\EVE\c_program_files_eve_sharedcache_sisi_singularity\settings_Default")
 #-------------end of your info--------------------------
 
-def replace(files, char_master_path, user_master_path, dest_dir):
-                    for file in files:
-                        
-                        if "core_char" in file and not char_master in file:
-                                shutil.copy(char_master_path, os.path.join(dest_dir, file))
-                                print("Overwriten: {}".format(file))
+def replace(char_master_path, user_master_path, dest_dir):
+    for root, dirs, files in os.walk(dest_dir):
+        for file in files:
+            destination_path = os.path.join(dest_dir, file)
 
-                        if "core_user" in file and not user_master in file:
-                                shutil.copy(user_master_path, os.path.join(dest_dir, file))
-                                print("Overwriten: {}".format(file))
+            if "core_char" in file:
+                if file.split("_")[2] not in exclude_chars and char_master_path != destination_path:
+                    shutil.copy(char_master_path, os.path.join(dest_dir, file))
+                    print(f"Overwriten character file: {file}")
+            
+            elif "core_user" in file:
+                if file.split("_")[2] not in exclude_users and user_master_path != destination_path:
+                    shutil.copy(user_master_path, os.path.join(dest_dir, file))
+                    print(f"Overwriten user file: {file}")
 
 # Generating Paths to coresponding files
-char_master_path = os.path.join(tq_dir, "core_char_{}.dat".format(char_master))
-user_master_path = os.path.join(tq_dir, "core_user_{}.dat".format(user_master))
+char_master_path = os.path.join(tq_dir, f"core_char_{char_master}.dat")
+user_master_path = os.path.join(tq_dir, f"core_user_{user_master}.dat")
 
 try: # Try-except block, so that in case of an error you still get some output, that I might debug at some point
-        print("Tranquility")
-        for root, dirs, files in os.walk(tq_dir):
-                replace(files, char_master_path, user_master_path, tq_dir)
+    print("Tranquility")
+    replace(char_master_path, user_master_path, tq_dir)
 
-        # Comment out the next 3 lines if you dont want to copy the settings to singularity              
-        print("Singularity")
-        for root, dirs, files in os.walk(sisi_dir):
-                replace(files, char_master_path, user_master_path, sisi_dir)
+    # Comment out the next 3 lines if you dont want to copy the settings to singularity              
+    print("Singularity")
+    replace(char_master_path, user_master_path, sisi_dir)
                 
 except Exception as e:
-        print("An Error occurred while: copying: {}".format(str(e)))
+    raise
+    print("An Error occurred while: copying: {}".format(str(e)))
 else:	
-        print("Done")
+    print("Completed succesfully")
 
 # Comment out this last line if you run it by script and want the shell to auto-close
 input("Press any key to continue ...")
-	
